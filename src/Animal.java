bd9817b13bbd8b7c982a066ed1480cd8947881c2
juli4ndrade <juliaandrde52@gmail.com>
2023-05-26 09:55:53 -0300

    atividade

--- src/Animal.java
+++ src/Animal.java
@@ -1,6 +1,15 @@
 public class Animal {
-    // Definir 2 ou mais atributos
+    // Atributos
+    String especie;
+    String porte;
+    int idade;
+    String raca;
+    String corPelo;
+    String corOlhoDireito;
+    String nome;
+    // Métodos
+    public void comer(){
+        System.out.println("Animal comeu");
+    }
 
-    // Definir 1 ou mais métodos
-    
 }
